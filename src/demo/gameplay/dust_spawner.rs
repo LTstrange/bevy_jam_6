195fce5c2b67ccc137e9a26b4f458dc5e6bbbe65
LTstrange <835422774@qq.com>
2025-06-06 20:35:04 +0800

    refactor dust spawn

--- src/demo/gameplay/dust_spawner.rs
+++ src/demo/gameplay/dust_spawner.rs
@@ -1,32 +1,36 @@
+use std::time::Duration;
+
 use super::dust::dust;
 use crate::{demo::GAME_AREA, prelude::*};
 
 pub(super) fn plugin(app: &mut App) {
-    app.insert_resource(DustSpawnerStats {
-        dust_spawn_speed: 2, // Default spawn speed
-    });
     app.add_systems(Update, tick_spawner_timer.in_set(AppSystems::TickTimers));
     app.add_systems(Update, (spawn_dust,).in_set(AppSystems::Update));
 
     app.add_systems(Update, gizmos.run_if(in_state(Screen::Gameplay)));
-}
 
-// TODO: make use of it
-#[derive(Resource, Reflect, Debug, Default)]
-#[reflect(Resource)]
-struct DustSpawnerStats {
-    dust_spawn_speed: u32, // Number of dust particles spawned per second
+    app.add_observer(
+        |t: Trigger<SetDustSpawnStats>, mut dust_spawner: Single<&mut DustSpawner>| {
+            let SetDustSpawnStats::SpawnSpeed(speed) = t.event();
+            dust_spawner
+                .timer
+                .set_duration(Duration::from_secs_f32(1.0 / *speed));
+        },
+    );
 }
 
 pub fn dust_spawner() -> impl Bundle {
     (
         Name::new("Dust Spawner"),
         StateScoped(Screen::Gameplay),
-        DustSpawner {
-            timer: Timer::from_seconds(0.5, TimerMode::Repeating),
-        },
+        DustSpawner::new(2.0),
         Entropy::<WyRand>::default(),
     )
+}
+
+#[derive(Event, Debug, Clone)]
+pub enum SetDustSpawnStats {
+    SpawnSpeed(f32), // Change the spawn speed of dust particles
 }
 
 #[derive(Component, Reflect, Debug)]
@@ -35,21 +39,26 @@ struct DustSpawner {
     timer: Timer,
 }
 
-fn tick_spawner_timer(mut spawner: Query<&mut DustSpawner>, time: Res<Time>) {
-    for mut spawner in spawner.iter_mut() {
-        spawner.timer.tick(time.delta());
+impl DustSpawner {
+    pub fn new(spawn_speed: f32) -> Self {
+        Self {
+            timer: Timer::from_seconds(1.0 / spawn_speed, TimerMode::Repeating),
+        }
     }
 }
 
-fn spawn_dust(mut commands: Commands, spawners: Query<(&mut DustSpawner, &mut Entropy<WyRand>)>) {
-    for (spawner, mut entropy) in spawners {
-        if spawner.timer.just_finished() {
-            let pos = Vec2::new(
-                entropy.random_range(GAME_AREA.min.x..GAME_AREA.max.x),
-                GAME_AREA.max.y,
-            );
-            commands.spawn(dust(pos, entropy.random_range(80.0..120.0)));
-        }
+fn tick_spawner_timer(mut spawner: Single<&mut DustSpawner>, time: Res<Time>) {
+    spawner.timer.tick(time.delta());
+}
+
+fn spawn_dust(mut commands: Commands, spawners: Single<(&mut DustSpawner, &mut Entropy<WyRand>)>) {
+    let (spawner, mut entropy) = spawners.into_inner();
+    if spawner.timer.just_finished() {
+        let pos = Vec2::new(
+            entropy.random_range(GAME_AREA.min.x..GAME_AREA.max.x),
+            GAME_AREA.max.y,
+        );
+        commands.spawn(dust(pos, entropy.random_range(80.0..120.0)));
     }
 }
 
